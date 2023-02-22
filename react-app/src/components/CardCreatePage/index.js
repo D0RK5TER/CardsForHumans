3e00b21e4c6d4e778c1131624a154570c60f6ce8
P. Dexter Assaf <107891735+D0RK5TER@users.noreply.github.com>
2023-02-21 19:59:03 -0800

    increased seed data

--- react-app/src/components/CardCreatePage/index.js
+++ react-app/src/components/CardCreatePage/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-export default function OneCard({ id }) {
+export default function CardCreate({ deck }) {
     // const sessionUser = useSelector(state => state.user);
     return (
         <div>
