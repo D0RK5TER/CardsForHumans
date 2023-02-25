488d3c232756f3ca2dccfdfd015d5d06a857a42c
P. Dexter Assaf <107891735+D0RK5TER@users.noreply.github.com>
2023-02-24 21:52:54 -0800

    card display sorta done, still buggy

--- react-app/src/components/CardDisplayCard/index.js
+++ react-app/src/components/CardDisplayCard/index.js
@@ -12,42 +12,58 @@ import DeckCard from '../DeckCard';
 import '../../0css/usercurr.css'
 import '../../0css/carddisplay.css'
 
-export default function CardDisplay({ cards, title, deck_id }) {
+export default function CardDisplay({ cards, title }) {
 
     const dispatch = useDispatch()
     const history = useHistory()
     const [cardmeme, setCardmeme] = useState(cards)
     const [a, b, c, ...rest] = cardmeme
+
+
     const handleIt = () => {
         let car = [b, c, ...rest]
         setCardmeme(car)
-        console.log('heyyyyy')
+        // console.log('heyyyyy')
     }
-    async function selectIt() {
-        // console.log(x)
-        if (title.includes('Pick')) {
-            // const data = await dispatch()
-        }
-        //dispath for add card to deck and then remove from cardmeme and continue
-    }
+    // const selectIt = () => {
+    //     console.log('yyyyyyyeh')
+    //     setCardmeme(cardmeme)
+    // }
+    //     // e.preventDefault()
+    //     // // console.log(x)
+    //     // console.log('enter here')
+    //     // console.log(title)
+    //     // if (title.includes('Pick')) {
+    //     const data = await dispatch(thunkMyInfo())
+    //     //     console.log('enter here')
+    //     setCardmeme(data)
+    //     // }
+    //     // return 0
+    //     //dispath for add card to deck and then remove from cardmeme and continue
+    // }
     return (
         <div id='card-display-whole'>
             <div id='card-display-header'>
                 {title}
             </div>
             <div className='card-display-cards'>
-                <div onClick={selectIt}>
+
+                <div>
                     <CardCard card={a} make={1} />
                 </div>
+
                 <div>
                     <CardCard card={b} make={1} />
                 </div>
                 <div>
                     <CardCard card={c} make={1} />
                 </div>
-                {rest?.length ? <div id='display-next' onClick={handleIt}>
-                    ►
-                </div> : <></>}
+                <div>
+
+                    {rest?.length ? <div id='display-next' onClick={handleIt}>
+                        ►
+                    </div> : <></>}
+                </div>
             </div>
 
         </div>
