7cb98b4a138ae4dbf6059c57b3c0668a70bc44f4
P. Dexter Assaf <107891735+D0RK5TER@users.noreply.github.com>
2023-03-16 23:20:33 -0700

    getting ready for grad

--- react-app/src/0utils/funcs.js
+++ react-app/src/0utils/funcs.js
@@ -1,5 +1,5 @@
 export function ageMinutes(d) {
-    console.log(d)
+    // console.log(d)
     let today = new Date().getTime()
     let mili = today % new Date(d).getTime()
     return Math.floor(mili/60000)
